d396129d5a1f6b79269ca9d6192429052d669bbe
Krish Choudhary <kdhakla333@gmail.com>
2023-12-31 12:54:51 +0530

    Find pivot elements using binary search

--- 043pivot.cpp
+++ 043pivot.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
-int getPivotElement(int arr[],int size){
-    int start=0, end=size-1, mid=start+(end-start)/2;
+int getPivotElement(int arr[], int size){
+    int start = 0, end = size - 1, mid = start + (end - start) / 2;
 
-    while(start<end){
-        if(arr[mid]>=arr[0]){    //element lie on line 1 (reference to notes)
-            start=mid+1;
+    while(start < end){
+        if(arr[mid] >= arr[0]){    //element lie on line 1 (reference to notes)
+            start = mid + 1;
         }
         else{                    // element lie on line 2
-            end=mid;
+            end = mid;
         }
-        mid=start+(end-start)/2;
+        mid = start + (end - start) / 2;
     }
     // In case of pivot element start=end
     // So we can return any 1 of start or end
@@ -18,8 +18,8 @@ int getPivotElement(int arr[],int size){
 }
 
 int main(){
-    int arr[5]={3,8,10,1,2};
-    int pivot=getPivotElement(arr,5);
-    cout<<"Pivot element is: "<<arr[pivot];
+    int arr[5] = {3, 8, 10, 1, 2};
+    int pivot = getPivotElement(arr, 5);
+    cout<<"Pivot element is: "<< arr[pivot];
     return 0;
 }
