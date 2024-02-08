e72255ab0423fc548d8e70de85acc16e6f3fa1b3
Krish Choudhary <kdhakla333@gmail.com>
2024-02-08 23:39:13 +0530

    LinkedList Method

--- 127implementAQueue.cpp
+++ 127implementAQueue.cpp
@@ -53,3 +53,106 @@ public:
         return arr[qfront];
     }
 };
+
+/*
+    Time complexity: O(q)
+    Space complexity: O(q)
+
+    Where q is the number of queries.
+*/
+
+class Node {
+public :
+    int data;
+    Node *next;
+
+    Node(int data) {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+class QueueViaLinkedList {
+public:
+
+    Node *head;
+    Node *tail;
+    int size;
+
+    QueueViaLinkedList() {
+        head = NULL;
+        tail = NULL;
+        size = 0;
+    }
+
+    ~QueueViaLinkedList() {
+        Node *temp = head;
+        while (temp)
+        {
+            Node *t = temp;
+            temp = temp->next;
+            delete t;
+        }
+    }
+
+    // Function to check if the queue is empty.
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    void enqueue(int data) {
+
+        // Increase the count of elements present in the List.
+        size++;
+
+        // Create a new node.
+        Node *newNode = new Node(data);
+
+        // Check if the Queue is empty.
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+
+        // Push the element to the last of the queue.
+        tail->next = newNode;
+        tail = newNode;
+        return;
+    }
+
+    int dequeue() {
+
+        // Check if the queue is empty.
+        if (isEmpty()) {
+            return -1;
+        }
+
+        int ans = head->data;
+
+
+        Node *tmp = head;
+        head = head->next;
+
+        // If the queue is empty make the tail pointer NULL.
+        if (head == NULL) {
+            tail = NULL;
+        }
+        delete tmp;
+
+        size--;
+
+        return ans;
+    }
+
+    int front() {
+
+        // Check if the queue is empty.
+        if (isEmpty())  {
+            return -1;
+        }
+
+        // Return the element at the front.
+        return head->data;
+    }
+};
