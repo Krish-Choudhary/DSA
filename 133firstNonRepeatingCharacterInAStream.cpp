2fb8d6f49c57bae20adec0d5a76f060244186472
Krish Choudhary <kdhakla333@gmail.com>
2024-03-05 22:59:23 +0530

    first Non Repeating Character in a Stream

---
+++ 133firstNonRepeatingCharacterInAStream.cpp
@@ -0,0 +1,29 @@
+// https://www.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    unordered_map<char, int> count;
+		    queue<int> q;
+		    string ans;
+		    int n = A.length();
+		    for(int i = 0; i < n; i++){
+		        char ch = A[i];
+		        count[ch]++;
+		        if(count[ch] == 1)  q.push(ch);
+		        while(!q.empty()){
+		            if(count[q.front()] > 1)    q.pop();
+		            else{
+		                ans.push_back(q.front());
+		                break;
+		            }
+		        }
+		        if(q.empty())   ans.push_back('#');
+		    }
+		    return ans;
+		}
+
+};
