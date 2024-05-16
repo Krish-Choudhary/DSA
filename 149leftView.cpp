5997d00369bc6adcbc6eaea875b1477263a6f93d
Krish Choudhary <kdhakla333@gmail.com>
2024-05-16 16:24:49 +0530

    left View of a binary tree

---
+++ 149leftView.cpp
@@ -0,0 +1,36 @@
+// https://www.geeksforgeeks.org/problems/left-view-of-binary-tree/1
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+//Function to return a list containing elements of left view of the binary tree.
+void solve(Node* root, vector<int> &ans, int level) {
+    //base case
+    if(root == NULL)
+        return ;
+       
+    //we entered into a new level    
+    if(level == ans.size())
+        ans.push_back(root->data);
+        
+    solve(root->left, ans, level+1);
+    solve(root->right, ans, level+1);
+}
+
+vector<int> leftView(Node *root)
+{
+   vector<int> ans;
+   solve(root, ans, 0);
+   return ans;
+}
