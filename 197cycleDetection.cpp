355e5166f8d7ed810f117986e27dcb72f018e1e3
Krish Choudhary <kdhakla333@gmail.com>
2024-06-09 14:33:25 +0530

    Cycle Detection In Undirected Graph using DFS

--- 197cycleDetection.cpp
+++ 197cycleDetection.cpp
@@ -2,6 +2,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Using DFS
+bool isCyclicDFS(int node, int parent, unordered_map<int, bool> &visited, unordered_map<int, vector<int>> adjList){
+    
+    visited[node] = true;
+    
+    for(auto neighbour: adjList[node]){
+        if(!visited[neighbour]){
+            if(isCyclicDFS(neighbour, node, visited, adjList))  return true;
+        }
+        else if(neighbour != parent)    return true;
+    }
+    return false;
+}
+
+string cycleDetection (vector<vector<int>>& edges, int n, int m)
+{
+    unordered_map<int, vector<int>> adjList;
+    for(int i = 0; i < m; i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+
+    unordered_map<int, bool> visited;
+
+    for(int i = 0; i < n; i++){
+        if(!visited[i]) if(isCyclicDFS(i, -1, visited, adjList))    return "Yes";
+    }
+    return "No";
+}
+
 /*
 // using BFS
 bool isCyclicBFS(int source, unordered_map<int, bool> &visited, unordered_map<int, vector<int>> adjList){
