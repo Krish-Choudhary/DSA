db5ab6276cae5fcc86c7190e506999666ff95355
Krish Choudhary <kdhakla333@gmail.com>
2024-06-11 15:49:33 +0530

    Topological sort using Kahn's algorithm/BFS

--- 199topologicalSort.cpp
+++ 199topologicalSort.cpp
@@ -29,3 +29,37 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
     }
     return ans;
 }
+
+/*
+Using Kahn's algorithm / BFS
+#include <bits/stdc++.h> 
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    vector<vector<int>> adjList(v);
+    for(vector<int> i: edges)   adjList[i[0]].push_back(i[1]);
+
+    vector<int> inDegree(v, 0);
+    for(vector<int> i: adjList){
+        for(int j: i)   inDegree[j]++;
+    }
+    
+    // 0 indegree
+    queue<int> q;
+    for(int i = 0; i < v; i++){
+        if(inDegree[i] == 0)  q.push(i);
+    }
+
+    vector<int> ans;
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+        ans.push_back(front);
+
+        // neighbour indegree update
+        for(int neighbour: adjList[front]){
+            inDegree[neighbour]--;
+            if(inDegree[neighbour] == 0)    q.push(neighbour);
+        }
+    }
+    return ans;
+}
+*/
