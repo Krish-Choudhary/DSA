74b7bdc8a197e4fefbd2133e3a41dfcc8bfb4e8a
Krish Choudhary <kdhakla333@gmail.com>
2024-06-12 13:02:57 +0530

    Cycle detection in directed graph using Kahn's algorithm

--- 198cycleDetectionInDirectedGraph.cpp
+++ 198cycleDetectionInDirectedGraph.cpp
@@ -1,8 +1,7 @@
-// https://www.naukri.com/code360/problems/detect-cycle-in-a-directed-graph_1062626
 #include <bits/stdc++.h>
 using namespace std;
-
-bool checkCycleDFS(int node, map<int, bool> &visited, map<int, bool> &dfsVisited, map<int, vector<int>> adjList){
+/*
+bool checkCycleDFS(int node, map<int, bool> &visited, map<int, bool> &dfsVisited, map<int, vector<int>> &adjList){
   visited[node] = true;
   dfsVisited[node] = true;
 
@@ -39,4 +38,40 @@ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
         return 1;
   }
   return 0;
+}
+*/
+
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+    vector<vector<int>> adjList(n);
+    int m = edges.size();
+    for(int i = 0; i < m; i++){
+      int u = edges[i].first - 1;     // -1 to make it 0 based indexing
+      int v = edges[i].second - 1;
+      adjList[u].push_back(v);
+    }
+
+    vector<int> inDegree(n, 0);
+    for(vector<int> i: adjList){
+        for(int j: i)   inDegree[j]++;
+    }
+    
+    // 0 indegree
+    queue<int> q;
+    for(int i = 0; i < n; i++){
+        if(inDegree[i] == 0)  q.push(i);
+    }
+
+    int count = 0;
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+        count++;
+
+        // neighbour indegree update
+        for(int neighbour: adjList[front]){
+            inDegree[neighbour]--;
+            if(inDegree[neighbour] == 0)    q.push(neighbour);
+        }
+    }
+    return !(count == n);
 }
