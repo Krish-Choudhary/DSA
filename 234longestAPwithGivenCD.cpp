aa41d75ddf42a65686f690f7fc11c92d57dcabf1
Krish Choudhary <kdhakla333@gmail.com>
2024-08-05 18:29:50 +0530

    longest AP with given common difference

---
+++ 234longestAPwithGivenCD.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/description/
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int longestSubsequence(vector<int>& arr, int difference) {
+        unordered_map<int, int> dp;
+        int ans = 0;
+        int n = arr.size();
+        for(int i = 0; i < arr.size(); i++){
+            int prev = arr[i] - difference;
+            int tempAns = 0;
+            if(dp.count(prev))  tempAns = dp[prev];
+            dp[arr[i]] = 1 + tempAns;
+            ans = max(ans, dp[arr[i]]);
+        }
+        return ans;
+    }
+};
