510c94009d4ac0f4422212f1957f8def5f114a29
Krish Choudhary <kdhakla333@gmail.com>
2024-08-05 18:32:19 +0530

    code correction

--- 234longestAPwithGivenCD.cpp
+++ 234longestAPwithGivenCD.cpp
@@ -9,7 +9,7 @@ public:
         unordered_map<int, int> dp;
         int ans = 0;
         int n = arr.size();
-        for(int i = 0; i < arr.size(); i++){
+        for(int i = 0; i < n; i++){
             int prev = arr[i] - difference;
             int tempAns = 0;
             if(dp.count(prev))  tempAns = dp[prev];
