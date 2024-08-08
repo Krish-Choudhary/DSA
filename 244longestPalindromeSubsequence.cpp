93641aa279978099e5edfc9ccebea053ea524051
Krish Choudhary <kdhakla333@gmail.com>
2024-08-08 20:13:58 +0530

    Longest Palindrome Subsequence

---
+++ 244longestPalindromeSubsequence.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/longest-palindromic-subsequence/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<int> next(text2.length() + 1, 0);
+        vector<int> curr(text2.length() + 1, 0);
+        for(int i = text1.length() - 1; i >= 0; i--){
+            for(int j = text2.length() - 1; j >= 0; j--){
+                int ans = 0;
+                if(text1[i] == text2[j])    ans = 1 + next[j + 1];
+                else    ans = max(curr[j + 1], next[j]);
+                curr[j] = ans;
+            }
+            next = curr;
+        }
+        return curr[0];
+    }
+public:
+    int longestPalindromeSubseq(string s) {
+        string s1 = s;
+        reverse(s.begin(), s.end());
+        return longestCommonSubsequence(s, s1);
+    }
+};
